eb0ac55acda30620b3d53bfd09d7641b1d66556a
gravoo <gravoo@yahoo.com>
2024-10-30 10:17:46 +0100

    fix for segfault on no args passing

--- main.cpp
+++ main.cpp
@@ -7,33 +7,39 @@
 int main(int argc, char **argv)
 {
 
-    int nodeCount=256-1;
-    int e=7,r=0;
-
-    Tree encodeTree{nodeCount};
-    Transmiter encoder{nodeCount,e,r,encodeTree};
-
-    Tree decodeTree{nodeCount};
-    Transmiter decoder{nodeCount,e,r,decodeTree};
-
-    try{
-
-        switch((char)*argv[1])
-        {
-            case 'e':
-                encoder.encode(argv[2]);
-                break;
-            case 'd':
-                decoder.decode(argv[2]);
-                break;
-            default:
-                std::cout<<"nie sprecyzowano czy kodowac czy dekodowac";
-                break;
-        }
-    }
-    catch(std::exception& e)
+    if(argc <= 2)
     {
-        std::cout << "No path specified " << e.what() << std::endl;
+        std::cout<<"no enough args passed\n";
+    }
+    else
+    {
+        int nodeCount=256-1;
+        int e=7,r=0;
+
+        Tree encodeTree{nodeCount};
+        Transmiter encoder{nodeCount,e,r,encodeTree};
+
+        Tree decodeTree{nodeCount};
+        Transmiter decoder{nodeCount,e,r,decodeTree};
+        try
+        {
+            switch((char)*argv[1])
+            {
+                case 'e':
+                    encoder.encode(argv[2]);
+                    break;
+                case 'd':
+                    decoder.decode(argv[2]);
+                    break;
+                default:
+                    std::cout<<"code or decode?\n";
+                    break;
+            }
+        }
+        catch(std::exception& e)
+        {
+            std::cout << "No path specified " << e.what() << std::endl;
+        }
     }
 
 
